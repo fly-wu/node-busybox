36bfa0945c6c8a84bcfbe095608095bf9414bf69
wuxifei <wuxifei@finupgroup.com>
2019-02-26 14:29:15 +0800

    update logic of findFileListByNameUpward

--- utils/local.js
+++ utils/local.js
@@ -3,7 +3,13 @@ const path = require('path');
 const HOME_PATH = process.env["HOME"];
 
 class LocalUtils {
-  findFileUpwards(dir, name) {
+
+  /**
+   * find file with name upwards
+   * @param dir, start dir
+   * @param name, target file
+   */
+  findClosestFile(dir, name) {
     let fullPath = path.resolve(dir, name);
     if (dir == HOME_PATH || dir == '/') {
       return null;
@@ -11,8 +17,24 @@ class LocalUtils {
     if (fs.existsSync(fullPath)) {
       return fullPath
     } else {
-      return findFileUpwards(path.resolve(dir, '..'), name)
+      return this.findClosestFile(path.resolve(dir, '..'), name)
     }
+  }
+
+  findFileListByNameUpward(dir, name) {
+    var results = [];
+
+    var currentPath = dir;
+    while (currentPath !== HOME_PATH && currentPath !== '/' && currentPath !== null) {
+      // console.log(currentPath);
+      var toFind = path.resolve(currentPath, name);
+      if (fs.existsSync(toFind)) {
+        results.push(toFind);
+      }
+      currentPath = path.resolve(currentPath, '..')
+    }
+
+    return results;
   }
 }
 
