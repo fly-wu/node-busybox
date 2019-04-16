07835c5e3bada28a7f8a9483b0e166fb75266228
wuxifei <wuxifei@finupgroup.com>
2019-04-16 17:18:41 +0800

    add node_module formidable for receive post data

--- tools/native-server/index.js
+++ tools/native-server/index.js
@@ -5,11 +5,16 @@ const http = require('http');
 const Stream = require('stream');
 const compose = require('koa-compose');
 const mimeTypes = require('mime-types');
+const formidable = require('formidable');
 const busybox = require('../../');
 
 class NativeServer {
-  constructor() {
-    this.STATIC_DIR = __dirname;
+  constructor(options = {
+    staticDir: null,
+    uploadDir: null,
+  }) {
+    this.STATIC_DIR = options.staticDir ? options.staticDir : process.cwd();
+    this.UPLOAD_DIR = options.uploadDir ? options.uploadDir : process.cwd();
   }
 
   resWritable(res) {
@@ -20,6 +25,68 @@ class NativeServer {
     // https://github.com/nodejs/node/blob/v4.4.7/lib/_http_server.js#L486
     if (!socket) return true;
     return socket.writable;
+  }
+
+  async parseByFormidable(ctx, next) {
+    const {req, res} = ctx;
+    const url = req.url;
+    if (!url.startsWith('/api/post')) {
+      return await next();
+    }
+    const uploadDir = this.uploadDir;
+
+    var form = new formidable.IncomingForm({
+      uploadDir,
+      keepExtensions: true,
+      hash: 'md5'
+    });
+
+    const multipart = await new Promise((resolve, reject) => {
+      // form.on('progress', (bytesReceived, bytesExpected) => {
+      //   console.log(`${bytesReceived} / ${bytesExpected}`);
+      // });
+      form.parse(ctx.req, (err, fields, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({
+            fields,
+            files
+          });
+        }
+      });
+    });
+
+    const resBody = {};
+    resBody.fields = multipart.fields;
+    resBody.files = {};
+
+    // mkdir uploads if necessary
+    var fileList = [];
+    Object.keys(multipart.files).forEach(key => {
+      fileList = fileList.concat(multipart.files[key]);
+    });
+    if (fileList.length > 0) {
+      const uploadDir = path.resolve(this.UPLOAD_DIR, 'uploads');
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+      }
+    }
+
+    // save files to local
+    Object.keys(multipart.files).forEach(key => {
+      resBody.files[key] = [];
+      const fileList = multipart.files[key];
+      fileList.forEach(file => {
+        resBody.files[key].push(file.name);
+        var ext = path.extname(file.name);
+        ext = ext.replace(/(\.[a-z0-9]+).*/i, '$1');
+        fs.writeFileSync(path.resolve(uploadDir, `${file.hash}${ext}`), file.data);
+      });
+    });
+    
+    ctx.type = 'json';
+    ctx.body = JSON.stringify(resBody);
   }
 
   // response assets file
@@ -102,7 +169,7 @@ class NativeServer {
 
   handleRequest(req, res) {
     const ctx = {req, res, status: 200, type: 'json', headers: {}};
-    const middleware = [this.responseAssets.bind(this), this.responseStaticFile.bind(this)];
+    const middleware = [this.parseByFormidable.bind(this), this.responseAssets.bind(this), this.responseStaticFile.bind(this)];
     const fnMiddleware = compose(middleware);
     fnMiddleware(ctx).then(() => {
       console.log(`process url: ${req.url}`);
