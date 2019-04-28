e806e732b8fdd5db52317f0cc3563afe1633c088
wuxifei <wuxifei@finupgroup.com>
2019-04-28 13:53:05 +0800

    add addModulesInDirNodeModules in class BusyBox

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// const busybox = {
-//   utils: require('./utils'),
-//   tools: require('./tools')
-// }
+const fs = require('fs');
+const path = require('path');
 const Utils = require('./utils');
 
 class BusyBox {
   constructor() {
     this.utils = new Utils(this);
+    this.addModulesInDirNodeModules();
+  }
+
+  addModulesInDirNodeModules() {
+    fs.readdirSync(path.resolve(__dirname, 'node_modules')).filter(it => !it.startsWith('.')).forEach(it => {
+      // console.log(`import model from file ${f}...`);
+      this[it] = require(path.resolve(__dirname, `node_modules/${it}`));
+    });
   }
 }
 
