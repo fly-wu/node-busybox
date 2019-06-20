a631508eea718e010263bdafb831b26652985f13
wuxifei <wuxifei@finupgroup.com>
2019-06-20 16:03:25 +0800

    change position of getFileListInFormatOfUl

--- utils/node.js
+++ utils/node.js
@@ -62,38 +62,6 @@ module.exports = class NodeUtils extends Common {
     return results;
   }
 
-  // return file list in the form of <ul><li></li></ul>
-  getFileListInFormatOfUl(dir) {
-    return new Promise((resolve, reject) => {
-      try {
-        const fileList = fs.readdirSync(dir);
-        const liList = Array.prototype.slice.call(fileList).map((it) => {
-          var item = '';
-          // // pass hidden file
-          // if (it.startsWith('.')) {
-          //   return item;
-          // }
-          const statInfo = fs.statSync(dir + '/' + it);
-          if (statInfo.isDirectory()) {
-            // item = '<li><a href="' + it + '/">' + it + '/</a></li>';
-            item = `<li><a href="${it}/">${it}/</a></li>`;
-          } else if (statInfo.isFile()) {
-            // item = '<li><a href="' + it + '">' + it + '</a></li>';
-            item = `<li><a href="${it}">${it}</a></li>`;
-          } else {
-            // item = '<li style="color: red"><a href="' + it + '">' + it + '</a></li>';
-            item = `<li style="color: red"><a href="${it}">${it}</a></li>`;
-          }
-          return item;
-        });
-        const ul = ['<ul>', ...liList, '</ul>'].join('');
-        resolve(ul);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
   // read dir recursive. return all files in dir root
   readDirRecursive(root, filter, files, prefix) {
     prefix = prefix || ''
@@ -342,6 +310,38 @@ module.exports = class NodeUtils extends Common {
       obj = {};
     }
     return obj;
+  }
+
+  // return file list in the form of <ul><li></li></ul>
+  getFileListInFormatOfUl(dir) {
+    return new Promise((resolve, reject) => {
+      try {
+        const fileList = fs.readdirSync(dir);
+        const liList = Array.prototype.slice.call(fileList).map((it) => {
+          var item = '';
+          // // pass hidden file
+          // if (it.startsWith('.')) {
+          //   return item;
+          // }
+          const statInfo = fs.statSync(dir + '/' + it);
+          if (statInfo.isDirectory()) {
+            // item = '<li><a href="' + it + '/">' + it + '/</a></li>';
+            item = `<li><a href="${it}/">${it}/</a></li>`;
+          } else if (statInfo.isFile()) {
+            // item = '<li><a href="' + it + '">' + it + '</a></li>';
+            item = `<li><a href="${it}">${it}</a></li>`;
+          } else {
+            // item = '<li style="color: red"><a href="' + it + '">' + it + '</a></li>';
+            item = `<li style="color: red"><a href="${it}">${it}</a></li>`;
+          }
+          return item;
+        });
+        const ul = ['<ul>', ...liList, '</ul>'].join('');
+        resolve(ul);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 
   /**
