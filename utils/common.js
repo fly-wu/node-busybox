f3ddf36891e4a08fd977948dbade15dc4c847662
wuxifei <wuxifei@finupgroup.com>
2019-08-27 18:38:19 +0800

    add function isPlainObject

--- utils/common.js
+++ utils/common.js
@@ -76,6 +76,27 @@
     isObject(val) {
       return val !== null && typeof val === 'object';
     }
+    isPlainObject(obj) {
+      var hasOwn = Object.prototype.hasOwnProperty;
+      var toStr = Object.prototype.toString;
+      if (!obj || toStr.call(obj) !== '[object Object]') {
+        return false;
+      }
+
+      var hasOwnConstructor = hasOwn.call(obj, 'constructor');
+      var hasIsPrototypeOf = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
+      // Not own constructor property must be Object
+      if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) {
+        return false;
+      }
+
+      // Own properties are enumerated firstly, so to speed up,
+      // if last one is own, then all properties are own.
+      var key;
+      for (key in obj) { /**/ }
+
+      return typeof key === 'undefined' || hasOwn.call(obj, key);
+    }
 
 
     isRegExp(obj) {
